e2cb255243e1b48c3b7fb7f6db5de979b133ce9b
Rahul Kumar <85149417+raHul-77k@users.noreply.github.com>
2024-06-10 14:57:11 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Sequelize = require('sequelize');
-const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{ 
+const sequelize=new Sequelize('expense','root','@#MaD.772k',{ 
     dialect:'mysql',
     host:process.env.DB_HOST,
 });
